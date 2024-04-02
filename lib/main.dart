9af84ff7a31ed65e48c7b89504bcde84e0dc2cb7
1rrasha <162358658+1rrasha@users.noreply.github.com>
2024-04-02 21:29:37 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,23 @@ class LoginPage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Rasha Login Page'),
+          title: Text(
+            'Rasha Login Page',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+              fontFamily: 'Arial', // Change to your desired font
+            ),
+          ),
+          centerTitle: true, // Center the title
           backgroundColor:
               Color.fromARGB(255, 201, 126, 155), // Set background color here
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.person), // Add user icon
+            ),
+          ],
         ),
         body: LoginForm(),
       ),
@@ -38,32 +52,57 @@ class _LoginFormState extends State<LoginForm> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          TextField(
-            controller: _usernameController,
-            decoration: InputDecoration(
-              labelText: 'Username',
-              prefixIcon: Icon(Icons.person),
+          Text(
+            'Login', // Add login label
+            style: TextStyle(
+              fontSize: 24.0, // Increase font size
+              fontWeight: FontWeight.bold, // Make it bold
+              color: Colors.blue, // Change color
             ),
           ),
           SizedBox(height: 20.0),
-          TextField(
-            controller: _passwordController,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              prefixIcon: Icon(Icons.lock),
-              suffixIcon: IconButton(
-                icon: _obscureText
-                    ? Icon(Icons.visibility_off)
-                    : Icon(Icons.visibility),
-                onPressed: () {
-                  setState(() {
-                    _obscureText = !_obscureText;
-                  });
-                },
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0), // Make rounded corners
+              border: Border.all(color: Colors.black), // Add border
+            ),
+            child: TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+                prefixIcon: Icon(Icons.person),
+                border: InputBorder.none, // Remove default border
+                contentPadding: EdgeInsets.all(10.0), // Add padding
               ),
             ),
-            obscureText: _obscureText,
-            maxLength: 20, // Limit the password to 20 characters
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0), // Make rounded corners
+              border: Border.all(color: Colors.black), // Add border
+            ),
+            child: TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: _obscureText
+                      ? Icon(Icons.visibility_off)
+                      : Icon(Icons.visibility),
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                ),
+                border: InputBorder.none, // Remove default border
+                contentPadding: EdgeInsets.all(10.0), // Add padding
+              ),
+              obscureText: _obscureText,
+              maxLength: 20, // Limit the password to 20 characters
+            ),
           ),
           SizedBox(height: 20.0),
           ElevatedButton(
